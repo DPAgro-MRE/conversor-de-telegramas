2329ecfc45b529d0b0c80961dcbfd7a1c692278f
eduardo.p.sousa <eduardopsousa28@gmail.com>
2024-11-29 12:51:33 -0300

    Nova abordagem de extração de informações; incompleta.

--- prototipo.py
+++ prototipo.py
@@ -5,20 +5,21 @@ import os
 import re
 import csv
 import PyPDF2
+from datetime import datetime
 
 TELsValidos = [] #Lista com as páginas ordenadas dos telegramas de caráter Ostensivo e Reservado, desconsiderando os Secretos.
 
 #Parte inicial do programa, recebe o PDF e extrai o texto das páginas.
 try:
-    diretorio = filedialog.askopenfilename(title="Selecione um arquivo PDF", filetypes=[("Arquivos PDF", "*.pdf")])
-    #diretorio = "C:/Users/eduardo.p.sousa/Downloads/GetFileAttachment.pdf"
+    #diretorio = filedialog.askopenfilename(title="Selecione um arquivo PDF", filetypes=[("Arquivos PDF", "*.pdf")])
+    diretorio = "C:/Users/eduardo.p.sousa/Downloads/TELDODIAT.pdf"
     with open(diretorio, 'rb') as arquivoPdf:
         leitor = PyPDF2.PdfReader(arquivoPdf)
         textoPdf = ""
         for pagina in leitor.pages:
             textoPdf += pagina.extract_text()
-            textoPdf = textoPdf.split("\n") #Separação da página por quebra de linhas
-            if ("Para:" in textoPdf[1]) or ("CARAT=Secreto" in textoPdf[2]) or (len(textoPdf)<7):
+            textosPdf = textoPdf.split("\n") #Separação da página por quebra de linhas
+            if ("Para:" in textosPdf[1]) or ("CARAT=Secreto" in textosPdf[2]) or ('Distribuído' not in textoPdf):
                 pass
             else:
                 TELsValidos.append(pagina.extract_text())
@@ -27,9 +28,23 @@ except Exception as e:
     print(f"Erro ao ler o PDF: {e}")
 arquivoPdf.close()
 
+def gerarExcel(nome_arquivo, dados):
+    try:
+        workbook = Workbook()
+        planilha = workbook.active
+        planilha.title = "Coleção"
+        planilha.append(["data_hora_entrada", "data_documento", "tipo_documento", "numero", "ano", "remetente", "documento", "indice", "prioridade", "carater", "distribuicao", "primdistribuicao", "redistribuicao", "refdoc", "processos_sei", "teor", "corpo", "resumo", "paises_ois", "pasta", "apenas_cumpre_instrucoes"])
+        for linha in dados:
+            planilha.append(linha)
+        workbook.save(nome_arquivo+".xlsx")
+        caminhoExcel = os.path.abspath(nome_arquivo+".xlsx")
+        print(f"Arquivo Excel criado em: {caminhoExcel}")
+    except Exception as e:
+        print(f"Erro ao criar o arquivo Excel: {e}")
+
 def gerarTxt(textoPdf):
     try:
-        nome_arquivo_txt = os.path.basename(diretorio).replace(".pdf","") +f"{cont}" ".txt" 
+        nome_arquivo_txt = (f"TEL{NumTel}.txt") 
         with open(nome_arquivo_txt, 'w', encoding='utf-8-sig') as arquivo_txt:
             if type(textoPdf) is list:
                 for pagina in textoPdf:
@@ -44,35 +59,41 @@ def gerarTxt(textoPdf):
 #Segunda parte do programa, separa os telegramas válidos por grupos de página.
 TEL = []
 Dados = []
-cont = 0 #temp
 for pagina in TELsValidos:
     numero_pagina = (((pagina.split("\n"))[0]).split(" "))
     numero_pagina = numero_pagina[1]
     texto_Tel = ""
-    #Pega primeiramente as informações que são garantidas de aparecer em ordem nos telegramas.
+    #Utiliza padrões de regex para extrair as informações das páginas.
     if int(numero_pagina[0])//int(numero_pagina[2]) == 1:
         TEL.append(pagina)
         for i in range(len(TEL)):
-            pagina_tel = TEL[i].split("\n")
-            #pagina_tel[10].startswith("DISTR=DPAGRO")
             if i == 0:
-                matchRemetenteData = re.search(r"De (.*?) para Exteriores em (\d{2}/\d{2}/\d{4})", pagina_tel[3])
+                matchDistr = re.search(r'DISTR=([A-Za-z/]+)', TEL[i])
+                if not matchDistr.group(1).startswith("DPAGRO"):
+                    break
+                DISTR = matchDistr.group(1) #CHECK
+                matchRemetenteData = re.search(r"De (.*?) para Exteriores em (\d{2}/\d{2}/\d{4})", TEL[i])
                 Remetente = matchRemetenteData.group(1)     #CHECK
                 DataExpedicao = matchRemetenteData.group(2) #CHECK
-                matchNumTel = re.search(r"De: .*? Recebido em: (\d{2}/\d{2}/\d{4}) (\d{2}:\d{2}:\d{2}) N.°: (\d{5})", pagina_tel[1])
+                matchNumTel = re.search(r"De: .*? Recebido em: (\d{2}/\d{2}/\d{4}) (\d{2}:\d{2}:\d{2}) N.°: (\d{5})", TEL[i])
                 DataRecebimento = matchNumTel.group(1) #CHECK
                 HoraEntrada = matchNumTel.group(2) #CHECK
                 NumTel = matchNumTel.group(3) #CHECK
-                CARAT = (pagina_tel[6].replace("CARAT=", ""))
-                PRIOR = (pagina_tel[9].replace("PRIOR=", ""))
-                DISTR = (pagina_tel[10].replace("DISTR=", ""))
-                cont += 1 #temp
-                gerarTxt(TEL)
+                matchCarat = re.search(r'CARAT=([A-Za-z]+)', TEL[i])
+                matchPrior = re.search(r'PRIOR=([A-Za-z]+)', TEL[i])
+                CARAT = matchCarat.group(1) #CHECK
+                PRIOR = matchPrior.group(1) #CHECK
+                PRIMDISTR = "DPAGRO"
+                #data = datetime.strptime(data_string, "%d-%m-%Y %H:%M:%S") não está pronto
+                Dados.append([DataRecebimento, DataExpedicao, "TEL", NumTel, "2024", Remetente, "documento", "indice", PRIOR, CARAT, DISTR, PRIMDISTR,"redistribuicao", "refdoc", "processos_sei", "teor", "corpo","resumo","paises_ois","pasta","apenas_cumpre_instrucoes"])
+        
         #Começar as operações com o telegrama
         TEL.clear()
     elif int(numero_pagina[0])//int(numero_pagina[2]) != 1:
         TEL.append(pagina)
+gerarExcel("TELDODIAM", Dados)
 
+'''
 #Função utilizada para gerar o Csv utilizando os dados em formato de lista aninhada.
 def gerarCsv(nome_arquivo, dados):
     try:
@@ -99,11 +120,8 @@ def gerarExcel(nome_arquivo, dados):
         print(f"Arquivo Excel criado em: {caminhoExcel}")
     except Exception as e:
         print(f"Erro ao criar o arquivo Excel: {e}")
-#gerarTxt(TELsValidos)
 
-'''
-Função para gerar um .txt sendo informado uma string ou lista, não permanecerá no final do projeto e serve apenas para testes.
-
+#Função para gerar um .txt sendo informado uma string ou lista, não permanecerá no final do projeto e serve apenas para testes.
 def gerarTxt(textoPdf):
     try:
         nome_arquivo_txt = os.path.basename(diretorio).replace(".pdf","") + ".txt"
